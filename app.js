29f8fb13b6b79b048ace5af4526bb6f9f04e0bcf
BfortF5 <brettfort@hotmail.com>
2023-10-14 14:12:47 -0700

    updating baseball

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let userFrom = prompt("Where are you from?");
 if(userFrom == 'Seattle'){
     document.write('Wow, your favorite team must be a Seattle Marnier or Seahawlk fan.');
 } else {
-    document.write(Welcome from 'userFrom'!!!)
+    document.write("Welcome from " + userFrom + "!!!");
 }
 
 
