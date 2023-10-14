634cf16fc5233e3e110dfd317ee6668c4d91ce99
BfortF5 <brettfort@hotmail.com>
2023-10-14 13:43:46 -0700

    updating changes

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ document.write('Hello!, welcome to my Baseball Card Website!!!');
 
 let userFrom = prompt("Where are you from?");
 
-document.write(Welcome from 'userFrom'!!!)
+if(userFrom == 'Seattle'){
+    document.write('Wow, your favorite team must be a Seattle Marnier or Seahawlk fan.');
+} else {
+    document.write(Welcome from 'userFrom'!!!)
+}
+
+
+
 
 
